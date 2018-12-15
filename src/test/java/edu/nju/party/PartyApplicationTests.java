83edc8a89283ce17adc2e195dd67cbc41e5624a5
finominon <zcj14@software.nju.edu.cn>
2018-12-15 22:00:33 +0800

    create a respository

---
+++ src/test/java/edu/nju/party/PartyApplicationTests.java
@@ -0,0 +1,18 @@
+package edu.nju.party;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PartyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+}
+
